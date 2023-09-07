95b7b6af9c8009a7f48405583fd7adda60574c66
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-09-07 09:26:05 +0530

    👑KING-VAJIRA👑

--- config.js
+++ config.js
@@ -4,11 +4,11 @@ const fs = require('fs-extra')
   
  //═══════[Required Variables]════════\\ 
  global.owner = process.env.OWNER_NUMBER.split(",") 
- global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority" 
+ global.mongodb = process.env.MONGODB_URI || "mongodb+srv://vajira:vajira@cluster0.hzwqrqq.mongodb.net/?retryWrites=true&w=majority" 
  global.port= process.env.PORT || 5000 
  global.email = 'sam@secktor.live' 
- global.github = 'https://github.com/nipuna15' 
- global.location = 'Sri lanka, Monaragala' 
+ global.github = 'https://github.com/vajirabot1' 
+ global.location = 'Sri lanka, Rathnapura' 
  global.gurl = 'https://instagram.com/' // add your username 
  global.sudo = process.env.SUDO || '919628516236' 
  global.devs = '919628516236'; 
@@ -30,7 +30,7 @@ voice  :  process.env.AUTO_VOICE === undefined ? false : process.env.AUTO_VOICE,
    auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER, 
    HANDLERS:  process.env.PREFIX === undefined ? ['.'] : process.env.PREFIX, 
    warncount : process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT, 
-   disablepm:  process.env.DISABLE_PM === undefined ? false : process.env.DISABLE_PM, 
+   disablepm:  process.env.DISABLE_PM === undefined ? true : process.env.DISABLE_PM, 
    levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? false : process.env.LEVEL_UP_MESSAGE, 
    antilink:  process.env.ANTILINK_VALUES === undefined ? 'chat.whatsapp.com' : process.env.ANTILINK_VALUES, 
    antilinkaction: process.env.ANTILINK_ACTION === undefined ? 'remove' : process.env.ANTILINK_ACTION, 
